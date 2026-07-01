866c9e4adf00c0a95ce14be806bd999eb025f8f4
Kettarian <Nikita.protsanov@gmail.com>
2026-07-01 21:26:58 +0300

    pretrain-ready

--- src/gpt.py
+++ src/gpt.py
@@ -49,7 +49,7 @@ class TransformerBlock(nn.Module):
             d_in = cfg['emb_dim'],
             d_out = cfg['emb_dim'],
             context_length=cfg['context_length'],
-            num_heads=cfg['num_heads'],
+            num_heads=cfg['n_heads'],
             dropout=cfg['drop_rate'],
             qkv_bias=cfg['qkv_bias']
         )
@@ -105,3 +105,17 @@ class GPTModel(nn.Module):
         logits = self.out_head(x)
 
         return logits
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
